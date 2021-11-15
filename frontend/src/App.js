cce3a8bf36bdb449beac23e857319562349bb1c0
fuminaInoue <fumina.mm@gmail.com>
2021-11-15 17:29:13 +0900

    基盤作成

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState,useEffect } from 'react'
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
+  return (
+    <div>
+<p>{ message }</p>    </div>
+  );
+}
+
+export default App;
