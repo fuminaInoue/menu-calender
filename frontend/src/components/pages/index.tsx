6de9cf38adf1c08afb5887cf39b82e4fa54112d9
fuminaInoue <fumina.mm@gmail.com>
2022-01-21 18:41:52 +0900

    emotion導入

--- frontend/src/components/pages/index.tsx
+++ frontend/src/components/pages/index.tsx
@@ -1,12 +1,20 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react'
+import { css, jsx } from '@emotion/react'
 
 type Props = {
 }
 
 export const Index: React.FC<Props> = ({}) => {
   return (
-    <div>
+    <div css={headerStyle}>
       Hello!
     </div>
   )
 }
+
+const headerStyle = css({
+  width: '100%',
+  background: 'pink',
+  height: 50
+})
